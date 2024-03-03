0e3bb550a96ed56e1a0244a1de482fd2cba954ee
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-03-03 23:11:20 +0300

    The right decision

--- src/main/java/ru/netology/services/Method.java
+++ src/main/java/ru/netology/services/Method.java
@@ -1,18 +1,19 @@
 package ru.netology.services;
+
 public class Method {
     public int calculate(int income, int expense, int threshold) {
         // income - доход от работы
         // expense - месячные обязательные траты
-        // threshold - денег на счету
-        // money - необходимое количество денег на счету
+        // threshold - необходимое количество денег на счету
+        int money = 0; // денег на счету изначально
         int count = 0; // счётчик месяцев отдыха изначально
         for (int month = 0; month < 12; month++) {   // с 1 по 12 месяц цикл с увеличением на 1 месяц
             // можем ли отдыхать?
-            if (20_000 >= threshold) { // Если денег на счету меньше чем нужно для отдыха тогда:
-                threshold = threshold + income - expense; // Денег на счету + доход - расходы
+            if (threshold >= money) { // Если денег на счету меньше чем нужно для отдыха тогда:
+                money = money + income - expense; // Денег на счету + доход - расходы
             } else {  // Если денег больше на счету чем нужно для отдыха тогда:
                 count++; // увеличиваем счётчик месяцев отдыха
-                threshold = (threshold - expense) / 3; //Денег на счету - траты на отдых
+                money = (money - expense) / 3; //Денег на счету - траты на отдых
             }
         }
         return count;  // верни количество месяцев отдыха
